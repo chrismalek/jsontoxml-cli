83410693a0b48c26cc053c161630636b48e16c4e
Chris Malek <chris.r.malek@gmail.com>
2016-03-22 11:29:56 -0700

    added some tests and changed structure of main to make it test able.

--- jsontoxml.go
+++ jsontoxml.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -11,39 +12,46 @@ import (
 )
 
 func main() {
+
 	var inputData []byte
+
 	if len(os.Args) > 1 {
-
 		inputData = []byte(os.Args[1])
-
 	} else {
-
 		inputData, _ = ioutil.ReadAll(os.Stdin)
 	}
 
 	if len(inputData) > 0 {
 
-		var parsedJson interface{}
-
-		if err := json.Unmarshal(inputData, &parsedJson); err != nil {
-			log.Println("invalid JSON found \n", inputData)
+		xmlout, err := convertJSONToXML(inputData)
+		if err != nil {
+			log.Println(err.Error())
 			os.Exit(1)
 		}
 
-		var xmlout []byte
-		anyxml.XMLEscapeChars(true)
-
-		xmlout, err2 := anyxml.Xml(parsedJson, "root")
-
-		if err2 != nil {
-			log.Println("Error converting to XML")
-			os.Exit(1)
-		}
-		fmt.Print(string(xmlout))
+		fmt.Println(string(xmlout))
 		os.Exit(0)
 
 	} else {
 		os.Exit(1)
 	}
+
+}
+
+func convertJSONToXML(inputData []byte) (xmlout []byte, err error) {
+	var parsedJson interface{}
+
+	if err := json.Unmarshal(inputData, &parsedJson); err != nil {
+		return []byte(""), errors.New(fmt.Sprintf("invalid JSON found %s\n", inputData))
+	}
+
+	anyxml.XMLEscapeChars(true)
+	xmlout, err2 := anyxml.Xml(parsedJson, "root")
+	fmt.Printf("convertJSONToXML-converted xml %s\n\n", string(xmlout))
+	if err2 != nil {
+		return []byte(""), errors.New("Error converting to XML")
+	}
+
+	return xmlout, nil
 
 }
